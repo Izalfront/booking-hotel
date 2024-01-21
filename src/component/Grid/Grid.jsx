2b5c62f70415af5b1cdb34b36cad7e3bdec11707
izalfront <rizalhajakubastarus@gmail.com>
2024-01-21 18:20:36 +0800

    develop grid 60%

--- src/component/Grid/Grid.jsx
+++ src/component/Grid/Grid.jsx
@@ -9,8 +9,17 @@ const Grid = () => {
     <div className="content__grid">
       {/* ==== grid 1 ===== */}
       <div className="grid__1">
-        <div className="desc__1">
-          <p>Ruang Rapat</p>
+        <div>
+          <div className="desc__1">
+            <p>Ruang Rapat</p>
+          </div>
+          <div className="line" style={{ marginLeft: '11rem' }}></div>
+          <div className="desc__detail">
+            <p>Dapatkan informasi lebih lanjut mengenai ruang pertemuan kami yang berlokasi di lantai khusus, dilengkapi ruangan meeting dengan berbagai ukuran.</p>
+            <a href="#" className="btn__jelajahi">
+              Jelajahi
+            </a>
+          </div>
         </div>
         <div className="img__content">
           <img src={Rapat} alt="" />
@@ -21,8 +30,11 @@ const Grid = () => {
         <div className="img__content2">
           <img src={Bermain} alt="" />
         </div>
-        <div className="desc__2">
-          <p>Ruang Bermain</p>
+        <div>
+          <div className="desc__2">
+            <p>Ruang Bermain</p>
+          </div>
+          <div className="line2" style={{ margin: '0', width: '15%', marginTop: '2rem' }}></div>
         </div>
       </div>
     </div>
