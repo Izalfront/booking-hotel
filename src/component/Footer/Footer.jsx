c3a55e29e2800b8375533296381562dd40ec246e
izalfront <rizalhajakubastarus@gmail.com>
2024-01-25 21:32:37 +0800

    completed develop footer

--- src/component/Footer/Footer.jsx
+++ src/component/Footer/Footer.jsx
@@ -1,8 +1,12 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react';
-import './footer.css';
+
 const Footer = () => {
-  return <div>footer</div>;
+  return (
+    <footer style={{ backgroundColor: '#f0f0f0', padding: '20px', textAlign: 'center' }}>
+      <p>&copy; 2024 Khan Place. Copyright Design by izalfront.</p>
+    </footer>
+  );
 };
 
 export default Footer;
