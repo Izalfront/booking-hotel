55b3835d852a0a6918cbe2cf453ff5420d14a4b5
izalfront <rizalhajakubastarus@gmail.com>
2024-01-26 15:29:29 +0800

    develop gridakomodasi completed, progress amenities 5%

---
+++ src/component/Amenities/Amenities.jsx
@@ -0,0 +1,8 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+
+const Amenities = () => {
+  return <div>FASILITAS</div>;
+};
+
+export default Amenities;
--- src/pages/Akomodasi.jsx
+++ src/pages/Akomodasi.jsx
@@ -2,6 +2,7 @@
 import React from 'react';
 import Akomodasi from '../component/Akomodasi/Akomodasi';
 import GridAkomodasi from '../component/GridAkomodasi/GridAkomodasi';
+import Amenities from '../component/Amenities/Amenities';
 const AkomodasiPage = () => {
   return (
     <>
@@ -9,6 +10,10 @@ const AkomodasiPage = () => {
       {/* ====== start akomodasi page ======= */}
       <section>
         <GridAkomodasi />
+      </section>
+      {/* ======= start fasilitas ======== */}
+      <section>
+        <Amenities />
       </section>
     </>
   );
