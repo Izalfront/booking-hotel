8e431d45e0ba4c23b5c341e78cc64cbcedb31ee7
izalfront <rizalhajakubastarus@gmail.com>
2024-01-27 19:19:30 +0800

    carousel head penawaran spesial completed

---
+++ src/component/PenawaranSpesial/PenawaranSpesial.jsx
@@ -0,0 +1,51 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+import Slider from 'react-slick';
+import HeadPenawaran from './penawaran.json';
+const PenawaranSpesial = () => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    autoplay: true,
+    speed: 1000,
+    swipeToSlide: true,
+    autoplaySpeed: 2000,
+    slidesToShow: 1,
+
+    responsive: [
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 2,
+          slideToScroll: 1,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 576,
+        settings: {
+          slidesToShow: 1,
+          slideToScroll: 1,
+        },
+      },
+    ],
+  };
+  return (
+    <>
+      <div className="hero__content">
+        <div className="img__background">
+          <Slider {...settings} className="slider__bg">
+            {HeadPenawaran.map((item, index) => (
+              <div className="img__child" key={index}>
+                <img src={item.image} alt={item.title} />
+              </div>
+            ))}
+          </Slider>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default PenawaranSpesial;
--- src/pages/PenawaranSpesial.jsx
+++ src/pages/PenawaranSpesial.jsx
@@ -1,8 +1,12 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react';
-
+import HeadPenawaran from '../component/PenawaranSpesial/PenawaranSpesial';
 const PenawaranSpesial = () => {
-  return <div>penawaran spesial</div>;
+  return (
+    <>
+      <HeadPenawaran />
+    </>
+  );
 };
 
 export default PenawaranSpesial;
