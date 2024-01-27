99777042e2b02438c1e14fb7e7fbc1d024a854f8
izalfront <rizalhajakubastarus@gmail.com>
2024-01-27 21:52:23 +0800

    develop gridfasilitas completed

--- src/component/GridFasilitas/GridFasilitas.jsx
+++ src/component/GridFasilitas/GridFasilitas.jsx
@@ -19,6 +19,15 @@ const GridFasilitas = () => {
           </div>
         </div>
       ))}
+      <div className="penawaran">
+        <h1>PENAWARAN SPESIAL</h1>
+        <p>
+          Promo-promo eksklusif untuk melengkapi pengalaman menginap Anda di Crowne Plaza Bandung. Berbagai promo mulai dari penawaran buffet dinner yang lezat, paket wedding spesial, hingga paket spa di Uluwatu Bali Spa, akan menjadi
+          penawaran yang sangat menarik.
+          <br />
+          Nikmati berbagai promo hotel terbaik di Bandung dan miliki pengalaman tak terlupakan di Crowne Plaza Bandung
+        </p>
+      </div>
     </>
   );
 };
