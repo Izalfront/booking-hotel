103131a6ff9d73401cf28a53c9367afa1b746a81
izalfront <rizalhajakubastarus@gmail.com>
2024-01-28 19:57:51 +0800

    develop rapat dan acara progress 60%

--- src/component/GridRapat/GridRapat.jsx
+++ src/component/GridRapat/GridRapat.jsx
@@ -1,6 +1,7 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react';
 import './gridrapat.css';
+import { BiBook } from 'react-icons/bi';
 import Kamar from '../../assets/img/kamar.jpg';
 const GridRapat = () => {
   return (
@@ -11,6 +12,24 @@ const GridRapat = () => {
             <div className="desc__rapat">
               <h1>Planning Tools</h1>
               <div className="line"></div>
+              <div className="document__rapat">
+                <div className="doc__1">
+                  <BiBook size={28} />
+                  <p style={{ paddingLeft: '1rem' }}>Layouts & Capacities</p>
+                </div>
+                <div className="doc__1">
+                  <BiBook size={28} />
+                  <p style={{ paddingLeft: '1rem' }}>Meeting Planning Guide</p>
+                </div>
+                <div className="doc__1">
+                  <BiBook size={28} />
+                  <p style={{ paddingLeft: '1rem' }}>Planning Timeline</p>
+                </div>
+                <div className="doc__1">
+                  <BiBook size={28} />
+                  <p style={{ paddingLeft: '1rem' }}>Expense Checklist</p>
+                </div>
+              </div>
             </div>
             <div className="img__rapat">
               <img src={Kamar} alt="" />
