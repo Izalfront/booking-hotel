6e95d0fd8a34f42ac2d85efa9953c22fbbcaf660
izalfront <rizalhajakubastarus@gmail.com>
2024-01-29 21:38:24 +0800

    develop rapat dan acara 90%

--- src/component/LayananBisnis/LayananBisnis.jsx
+++ src/component/LayananBisnis/LayananBisnis.jsx
@@ -1,8 +1,29 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react';
-
+import './layananbisnis.css';
+import { BiCog } from 'react-icons/bi';
+import { BiChalkboard } from 'react-icons/bi';
 const LayananBisnis = () => {
-  return <div>layanan bisnis</div>;
+  return (
+    <>
+      <div className="grid__layananbisnis">
+        <div className="service__bisnis">
+          <BiCog size={80} style={{ opacity: '0.5' }} />
+          <p>LAYANAN BISNIS</p>
+          <a href="#" className="btn__lebih">
+            lebih
+          </a>
+        </div>
+        <div className="peralatan__bisnis">
+          <BiChalkboard size={80} style={{ opacity: '0.5' }} />
+          <p>PERALATAN DAN PERLENGKAPAN PENUNJANG ACARA</p>
+          <a href="#" className="btn__lebih">
+            lebih
+          </a>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default LayananBisnis;
